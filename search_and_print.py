07a5a2ac365b94d97bee0aaea1621b9523d5313b
Matthew Cheng <matthewcheng88@gmail.com>
2018-01-12 10:22:58 -0800

    Starting Basic Search and Printing using Tweepy

---
+++ search_and_print.py
@@ -0,0 +1,19 @@
+#!usr/bin/python3
+import tweepy
+from tweepy import OAuthHandler
+import config
+
+if __name__ == "__main__":
+	auth = OAuthHandler(config.consumer_key, config.consumer_secret)
+	auth.set_access_token(config.access_token, config.access_secret)
+	api = tweepy.API(auth)
+
+	#Searching, where q is the search term. .items(5) restricts to only returning 5 results
+	search_results = tweepy.Cursor(api, q="Python").items(5)
+	for result in search_results:
+		print(result.text)
+
+	trends = api.trends_place(1)
+
+	for trend in trends[0]["trends"]:
+		print(trend['name'])
