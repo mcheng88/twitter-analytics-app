12981f90a12a33a77d7362399ad74964c72f91c9
Matthew Cheng <matthewcheng88@gmail.com>
2018-01-21 16:34:53 -0500

    Added new file, streaming_test.py, where I am trying to set up a streaming system to get real-time tweets.

---
+++ streaming_test.py
@@ -0,0 +1,24 @@
+import tweepy
+from tweepy.streaming import StreamListener
+from tweepy import Stream
+from tweepy import OAuthHandler
+import config
+import json
+
+class twitter_listener	(StreamListener):
+	def on_data(self, data):
+		print(data.text)
+		return True
+
+	def on_error(self, status):
+		print(status)
+
+if __name__ == "__main__":
+	auth = OAuthHandler(config.consumer_key, config.consumer_secret)
+	auth.set_access_token(config.access_token, config.access_secret)
+	api = tweepy.API(auth)
+	twitter_stream = Stream(auth, twitter_listener())
+	try:
+		twitter_stream.sample()
+	except Exception as e:
+		print(e.__doc__)
