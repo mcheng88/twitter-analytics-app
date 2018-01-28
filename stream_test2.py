dc261fdf3e8638107cbb79fd89f722c465c680d6
Matthew Cheng <matthewcheng88@gmail.com>
2018-01-27 21:00:23 -0500

    Added counter to the streaming, as well as implemented use of JSON.
    TODO: Research more about JSON.

--- stream_test2.py
+++ stream_test2.py
@@ -8,16 +8,45 @@ import json
 #override tweepy.StreamListener to add logic to on_status
 class MyStreamListener(tweepy.StreamListener):
 
-    def on_status(self, status):
-        print(status.text)
-        return True
-    def on_error(self, status):
-    	print(status.text)
-    	return False
+	# initializes the listener with a counter. 
+	def __init__(self, num_tweets):
+		self.counter = 0
+		self.num_tweets = num_tweets
+
+
+	# on data, prints tweet
+	def on_data(self, data):
+		try:
+			j = json.loads(data)
+			print(j["text"])
+			self.counter += 1
+			if self.counter == self.num_tweets:
+				return False
+
+			return True
+		except:
+			pass
+
+	# prints out tweet. 
+#    def on_status(self, status):
+#        print(status.text)
+#        return True
+
+	# on error, prints the error text.
+	def on_error(self, status):
+		print(status.text)
+		return False
 
 if __name__ == "__main__":
+	# For authentication purposes
 	auth = OAuthHandler(config.consumer_key, config.consumer_secret)
 	auth.set_access_token(config.access_token, config.access_secret)
 	api = tweepy.API(auth)
-	myStream = Stream(auth = api.auth, listener=MyStreamListener())
-	myStream.filter(track=['trump'], async=True)
+
+
+	myStream = Stream(auth = api.auth, listener=MyStreamListener(num_tweets=10))
+	try:
+		myStream.sample()
+	except Exception as e:
+		print(e.__doc__)
+#	myStream.filter(track=['trump'], async=True)
