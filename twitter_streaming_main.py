4f17fb0b4dbdb61c9a1a912620f639a2facaf573
Matthew Cheng <matthewcheng88@gmail.com>
2018-02-26 19:46:08 -0500

    Cleaned up code for twitter_streaming_main.py
    Beginning database creation to store the tweet data (create_database.py)
    
    Next steps: Adding database connection between main code and database.

--- twitter_streaming_main.py
+++ twitter_streaming_main.py
@@ -12,24 +12,24 @@ This is the listener class, will update with statistics and such
 class MyStreamListener(tweepy.StreamListener):
 
 	# initializes the listener with a counter. 
-	def __init__(self, num_tweets, retweet_count):
+	def __init__(self, num_tweets_to_grab, retweet_count):
 		self.counter = 0
-		self.num_tweets = num_tweets
+		self.num_tweets_to_grab = num_tweets_to_grab
 		self.retweet_count = retweet_count
 
 	# on data, prints tweet 
 	def on_data(self, data):
 		try:
-			j = json.loads(data)
-			print(j["text"])
+			json_data = json.loads(data)
+			# print(json_data["text"])
 			self.counter += 1
 			retweet_count = json_data["retweeted_status"]["retweet_count"]
 
 			if retweet_count >= self.retweet_count:
-				print(json_data["text"], retweet_count)
+				# print(json_data["text"], retweet_count)
 				self.stats.add_top_tweets(self.get_tweet_html(json_data['id']))
 
-			if self.counter >= self.num_tweets:
+			if self.counter >= self.num_tweets_to_grab:
 				return False
 
 			return True
@@ -47,31 +47,28 @@ class MyStreamListener(tweepy.StreamListener):
 		print(status.text)
 		return False
 
-
-
 """
 What if I set it up so that you could change the country code or the streaming search?
 """
 class TwitterMain():
-	def __init__(self, tweetsToGet, retweetNum):
+	def __init__(self, num_tweets_to_grab, retweet_count):
 		self.auth = OAuthHandler(config.consumer_key, config.consumer_secret)
 		self.auth.set_access_token(config.access_token, config.access_secret)
 
 		self.api = tweepy.API(self.auth)
-
-		self.num_tweets = tweetsToGet
-		self.retweet_count = retweetNum
+		self.num_tweets_to_grab = num_tweets_to_grab
+		self.retweet_count = retweet_count
 		self.stats = stats()
 
 	def get_streaming_data(self):
-		myStream = Stream(auth = self.api.auth, listener=MyStreamListener(self.num_tweets,
-		self.retweet_count))
+		myStream = Stream(auth = self.api.auth, listener=MyStreamListener(num_tweets_to_grab=self.num_tweets_to_grab, retweet_count=self.retweet_count))
 		try:
 			myStream.sample()
 		#	myStream.filter(track=['trump'])
 		except Exception as e:
 			print(e.__doc__)
 
+		top_tweets = self.stats.get_stats()
 		print(top_tweets)
 
 	def get_trends(self):
@@ -87,12 +84,12 @@ class TwitterMain():
 		trend_data = []
 
 		for trend in trends[0]["trends"]:
-			print(trend['name'])
+			# print(trend['name'])
 			trend_tweets=[]
 			trend_tweets.append(trend['name'])
 			tt = tweepy.Cursor(self.api.search, q = trend['name']).items(3)
 			for t in tt:
-				trend_tweets.append(t.text)
+				trend_tweets.append(self.get_tweet_html(t.id))
                 #print(tweet_html)
 			trend_data.append(tuple(trend_tweets))
 		print(trend_data)
